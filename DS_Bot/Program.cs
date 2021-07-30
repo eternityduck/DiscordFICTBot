9486dc870992eb747b5436d18d791e9aaaa3ea45
bohdan <bogdan2003bk96@gmail.com>
2021-07-30 13:11:39 +0300

    created first logic

--- DS_Bot/Program.cs
+++ DS_Bot/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
-
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 namespace DS_Bot
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient _client;
+	
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+            _client.MessageReceived += CommandsHandler;
+            _client.Log += Log;
+
+            var token = "ODcwNTk5NjUxMDQzMTg0NzE0.YQPHGw.uXN4Dy2S2R9YI2KiObIghFX9afA";
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            
+            await Task.Delay(-1);
+            Console.ReadLine();
+        }
+
+        private Task CommandsHandler(SocketMessage arg)
+        {
+            if (!arg.Author.IsBot)
+            {
+                switch (arg.Content)
+                {
+                    case "!hi":
+                        arg.Channel.SendMessageAsync($"Hi {arg.Author.Mention}");
+                        break;
+
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
